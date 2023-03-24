c61dbd5c3fe0361904492308f2e92c12010517d0
Xuhui Chen <severusheep9@hotmail.com>
2023-03-24 18:26:26 -0400

    bug fix: style change is not applied(still have bug in linethorugh & underline)

---
+++ jrteLib/src/commonMain/kotlin/com/jrte/ui/composable/JRTextScreen.kt
@@ -0,0 +1,47 @@
+package com.jrte.ui.composable
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.jrte.ui.textstyle.CustomStyleMapper
+import com.jrte.ui.textstyle.RichTextValue
+
+@Composable
+fun JRTextScreen(
+    modifier: Modifier = Modifier
+){
+    var value by remember {
+        mutableStateOf(
+            RichTextValue.fromString(
+                // Optional parameter; leave it blank if you want to use provided styles
+                // But if you want to customize the user experience you're free to do that
+                // by providing a custom StyleMapper
+                styleMapper = CustomStyleMapper()
+            )
+        )
+    }
+    val onValueChange: (RichTextValue) -> Unit = { value = it }
+    Column(modifier = modifier.fillMaxSize()) {
+        JRTextEditor(
+            value = value,
+            onValueChange = onValueChange,
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(16.dp),
+            textFieldStyle = defaultJRTextFieldStyle().copy(
+                textColor = Color.Black,
+                placeholderColor = Color.LightGray,
+                placeholder = "Write something here...",
+                textSize = 18.sp
+            ),
+        )
+    }
+    JRTextToolbar(
+        value = value,
+        onValueChange = onValueChange
+    )
+}
