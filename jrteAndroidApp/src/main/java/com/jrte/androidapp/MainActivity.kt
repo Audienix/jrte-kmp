4494deba8393d4da21a52422d97bb1e34da442c9
Xuhui Chen <severusheep9@hotmail.com>
2023-03-24 18:26:26 -0400

    bug fix: style change is not applied(still have bug in linethorugh & underline)

--- jrteAndroidApp/src/main/java/com/jrte/androidapp/MainActivity.kt
+++ jrteAndroidApp/src/main/java/com/jrte/androidapp/MainActivity.kt
@@ -4,18 +4,13 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.jrte.androidapp.ui.theme.JRTETheme
-import com.jrte.ui.composable.JRTextEditor
-import com.jrte.ui.composable.JRTextToolbar
-import com.jrte.ui.composable.defaultJRTextFieldStyle
+import com.jrte.ui.composable.JRTextScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,21 +21,7 @@ class MainActivity : ComponentActivity() {
             JRTETheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize()) {
-                    Column(modifier = Modifier.fillMaxSize()) {
-                        JRTextEditor(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .fillMaxHeight()
-                                .padding(16.dp),
-                            textFieldStyle = defaultJRTextFieldStyle().copy(
-                                textColor = Color.Black,
-                                placeholderColor = Color.LightGray,
-                                placeholder = "Write something here...",
-                                textSize = 18.sp
-                            ),
-                        )
-                    }
-                    JRTextToolbar()
+                    JRTextScreen()
                 }
             }
         }
@@ -50,8 +31,7 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun DefaultPreview() {
         JRTETheme {
-            JRTextEditor()
-            JRTextToolbar()
+            JRTextScreen()
         }
     }
 }
